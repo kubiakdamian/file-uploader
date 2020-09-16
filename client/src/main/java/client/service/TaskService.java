789334d6534f4f4e7729443e0ec6f458de2cb9ba
Damian Kubiak <kubiak.damian96@gmail.com>
2020-09-16 18:19:37 +0200

    Fixed problem with incorrect messages received by clients

--- client/src/main/java/client/service/TaskService.java
+++ client/src/main/java/client/service/TaskService.java
@@ -2,7 +2,6 @@ package client.service;
 
 import client.ClientData;
 import client.ClientListener;
-import client.FilesToSend;
 import client.QueuedFiles;
 import client.file.FileToProcess;
 import common.model.Dictionary;
@@ -40,14 +39,12 @@ public class TaskService {
         String name = keyboard.readLine();
         SignInUserTask task = new SignInUserTask(name);
         clientData.getServerUtils().sendTask(task);
-
         SignInServerResponse serverResponse = clientData.getServerUtils().fetchSignInServerResponse();
 
         if (serverResponse != null) {
             System.out.println(ServerResponse.SUCCESSFUL_SIGN_IN.getMessage());
             clientData.setDirectoryName(serverResponse.getDirectoryName());
             clientListener.start(clientData);
-            synchronizeFilesOnSignIn();
             return true;
         } else {
             System.out.println("Something went wrong, please try again");
@@ -66,22 +63,26 @@ public class TaskService {
     public void signUp() throws IOException {
         System.out.print("Enter your name and path directory divided by ':'. ex. john:johnFiles\n>");
         String taskData = keyboard.readLine();
-        String[] userData = taskData.split(":");
-        String userName = userData[0];
-        String directoryName = userData[1];
+        try {
+            String[] userData = taskData.split(":");
+            String userName = userData[0];
+            String directoryName = userData[1];
 
-        SignUpUserTask signUpUserTask = new SignUpUserTask(userName, directoryName);
-        clientData.getServerUtils().sendTask(signUpUserTask);
+            SignUpUserTask signUpUserTask = new SignUpUserTask(userName, directoryName);
+            clientData.getServerUtils().sendTask(signUpUserTask);
 
-        ServerResponse serverResponse = clientData.getServerUtils().fetchServerResponse();
+            ServerResponse serverResponse = clientData.getServerUtils().fetchServerResponse();
 
-        if (serverResponse == ServerResponse.SUCCESSFUL_SIGN_UP) {
-            clientData.setName(userName);
-            clientData.setDirectoryName(directoryName);
-            createDirectory(directoryName);
-            System.out.println(ServerResponse.SUCCESSFUL_SIGN_UP.getMessage());
-        } else {
-            System.out.println("Something went wrong, please try again");
+            if (serverResponse == ServerResponse.SUCCESSFUL_SIGN_UP) {
+                clientData.setName(userName);
+                clientData.setDirectoryName(directoryName);
+                createDirectory(directoryName);
+                System.out.println(ServerResponse.SUCCESSFUL_SIGN_UP.getMessage());
+            } else {
+                System.out.println("Something went wrong, please try again");
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Incorrect data");
         }
     }
 
@@ -100,23 +101,20 @@ public class TaskService {
         }
     }
 
-    public List<String> synchronizeFilesWithServer() {
+    public void synchronizeFilesWithServer() {
         GetFilenamesTask getFilenamesTask = new GetFilenamesTask();
         clientData.getServerUtils().sendTask(getFilenamesTask);
         FilesOnServerResponse filesOnServerResponse = clientData.getServerUtils().fetchFilesFromServer();
-        List<String> serverFileNames = new ArrayList<>();
 
         if (filesOnServerResponse != null) {
             System.out.println("Synchronizing files with server. Please wait...");
             List<String> localFiles = getLocalFiles();
-            serverFileNames = filesOnServerResponse.getFiles();
-            serverFileNames.removeAll(localFiles);
+            List<String> filesToCreate = filesOnServerResponse.getFiles();
+            filesToCreate.removeAll(localFiles);
 
-            printFilesToSynchronize(serverFileNames);
-            addAllFilesToQueue(serverFileNames);
+            printFilesToSynchronize(filesToCreate);
+            addAllFilesToQueue(filesToCreate);
         }
-
-        return serverFileNames;
     }
 
     private void createDirectory(String directoryName) throws IOException {
@@ -153,23 +151,6 @@ public class TaskService {
         System.out.println("Files to synchronize:");
         for (String file : files) {
             System.out.println(file);
-        }
-    }
-
-    private void synchronizeFilesOnSignIn() {
-        List<String> serverFiles = synchronizeFilesWithServer();
-        List<String> localFiles = getLocalFiles();
-        localFiles.removeAll(serverFiles);
-
-        if (!localFiles.isEmpty()) {
-            for (String file : localFiles) {
-                String[] fileData = file.split("#");
-                String name = fileData[0];
-                String size = fileData[1].replace(".txt", "");
-                common.model.file.File aFile = new common.model.file.File(name, Integer.parseInt(size));
-                aFile.setType(common.model.file.File.Type.CREATE);
-                FilesToSend.addFile(aFile);
-            }
         }
     }
 }
